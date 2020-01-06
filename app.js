4c268f820416e65b899e97822c3370f18398f79b
Łukasz Stefański <l.f.stefanski@gmail.com>
2020-01-06 23:02:40 +0100

    working for rock paper scissors

--- app.js
+++ app.js
@@ -24,23 +24,64 @@ rock.addEventListener( 'click', function() {
 
     choiceRock.setAttribute('class', 'far fa-hand-rock rockResult')
     youChoice.appendChild(choiceRock)
+    var newP = document.createElement('p')
+    newP.textContent = "Your choice"
+    youChoice.appendChild(newP)
+
 
     var choicePaper = paper.cloneNode(true)
     choicePaper.setAttribute('class', 'far fa-hand-paper paperResult')
     var choiceScissors = scissors.cloneNode(true)
+    choiceScissors.setAttribute('class', 'far fa-hand-paper scissorsResult')
+
 
     if(npcChoice === rockChoose){
-       cpuChoiceRock = choiceRock.cloneNode(true)
-       cpuChoice.appendChild(cpuChoiceRock)
-       
-       
-    }
-    // }else if(npcChoice === 2){
-    //     alert('Przegrałeś :(')
-    // }else if (npcChoice === 3){
-    //     alert('Wygrałeś')
-    // }
+        var drawSign = document.querySelector('.resultSign')
+        newSignP = document.createElement('p')
+        newSignP.textContent = ('=')
+        drawSign.appendChild(newSignP)
 
+       cpuChoiceRock = choiceRock.cloneNode(true)
+       var newP = document.createElement('p')
+        newP.textContent = "Cpu choice"
+       cpuChoice.appendChild(cpuChoiceRock)
+       cpuChoice.appendChild(newP)
+
+       var score = document.querySelector('.score')
+       score.textContent = ('DRAW')
+    
+    }else if(npcChoice === 2){
+        var loseSign = document.querySelector('.resultSign')
+        newSignP = document.createElement('p')
+        newSignP.textContent = ('>')
+        newSignP.style.color = 'red'
+        loseSign.appendChild(newSignP)
+
+       cpuChoicePaper = choicePaper.cloneNode(true)
+       var newP = document.createElement('p')
+        newP.textContent = "Cpu choice"
+       cpuChoice.appendChild(cpuChoicePaper)
+       cpuChoice.appendChild(newP)
+
+       var score = document.querySelector('.score')
+       score.textContent = ('YOU LOSE')
+    }
+    else if (npcChoice === 3){
+        var winSign = document.querySelector('.resultSign')
+        newSignP = document.createElement('p')
+        newSignP.textContent = ('<')
+        winSign.appendChild(newSignP)
+
+       cpuChoiceScissors = choiceScissors.cloneNode(true)
+       var newP = document.createElement('p')
+        newP.textContent = "Cpu choice"
+       cpuChoice.appendChild(cpuChoiceScissors)
+       cpuChoice.appendChild(newP)
+
+       var score = document.querySelector('.score')
+       score.textContent = ('YOU WIN')
+       score.style.color ='red'
+    }
 })
 
 paper.addEventListener('click', e => {
